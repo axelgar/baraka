83b825d2fba3999d44853a75ad44a25a7f4ae9c7
Francesca Ropolo <franci.ropolo@gmail.com>
2019-06-11 20:02:03 +0200

    f*cking show and hide

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// const card = document.getElementById('card-image');
+const cardsArray = document.querySelectorAll('.card');
+
+// ni puta idea de como hacerlo dinamico
+
+cardsArray.forEach((card, index) => {
+  const hideBtn = document.getElementById(`hide-btn-${index}`);
+  const showBtn = document.getElementById(`show-btn-${index}`);
+  const form = document.getElementById(`form-${index}`);
+
+  showBtn.addEventListener('click', () => {
+    form.classList.replace('hide', 'show');
+    card.classList.replace('card', 'hide');
+  });
+
+  hideBtn.addEventListener('click', () => {
+    form.classList.replace('show', 'hide');
+    card.classList.replace('hide', 'card');
+  });
+});
