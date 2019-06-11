8f6a4280a91a4aca6e40760011ee0ef4a57d9cdd
Francesca Ropolo <franci.ropolo@gmail.com>
2019-06-11 12:14:13 +0200

    logic for images

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,36 @@ const Image = require('../models/image.model');
 /* GET home page. */
 router.get('/', (req, res, next) => {
   Image.find()
-    .then((result) => {
-      const data = { images: result };
+    .then((results) => {
+      let mosaicos = [];
+      let deco = [];
+      let objetos = [];
+      let ceramicas = [];
+      let alfombras = [];
+      results.forEach(result => {
+        if (result.category === 'mosaicos') {
+          mosaicos.push(result);
+        }
+        if (result.category === 'objetos-singulares') {
+          objetos.push(result);
+        }
+        if (result.category === 'cerámica') {
+          ceramicas.push(result);
+        }
+        if (result.category === 'alfombras') {
+          alfombras.push(result);
+        }
+        if (result.category === 'decoración') {
+          deco.push(result);
+        }
+      });
+      const data = {
+        mosaicos,
+        deco,
+        objetos,
+        ceramicas,
+        alfombras
+      };
       console.log(data);
       res.render('index', data);
     })
