83eb443c6eeb37d618674189a0f639b2ccdbf8ea
Francesca Ropolo <franci.ropolo@gmail.com>
2019-06-13 18:15:39 +0200

    slider responsive

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -27,6 +27,26 @@ hideBtns.forEach(hideBtn => {
 const addBtn = document.querySelector('.add-new-btn');
 const newForm = document.querySelector('.add-new');
 
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+const carousel = document.querySelector('.carousel');
+
+if (arrowLeft && arrowRight && carousel) {
+  arrowLeft.onclick = () => {
+    if (carousel.scrollLeft === 0) {
+      carousel.scrollLeft += 3000;
+    }
+    carousel.scrollLeft -= carousel.offsetWidth;
+  };
+
+  arrowRight.onclick = () => {
+    if (carousel.scrollLeft >= (carousel.offsetWidth * 2)) {
+      carousel.scrollLeft = 0;
+    }
+    carousel.scrollLeft += carousel.offsetWidth;
+  };
+}
+
 addBtn.addEventListener('click', () => {
   newForm.classList.toggle('hide');
   newForm.classList.toggle('show');
