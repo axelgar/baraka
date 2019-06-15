a4385a654acc0c79c1c89b93db743da530db42e0
Axel Garcia <axel.garcia94@gmail.com>
2019-06-15 08:33:46 +0200

    add main function frontend

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,37 +1,34 @@
 'use strict';
 
-// const card = document.getElementById('card-image');
-const hideBtns = document.querySelectorAll('.hide-btn');
-const showBtns = document.querySelectorAll('.show-btn');
+function main () {
+  const hideBtns = document.querySelectorAll('.hide-btn');
+  const showBtns = document.querySelectorAll('.show-btn');
 
-showBtns.forEach(showBtn => {
-  showBtn.addEventListener('click', e => {
-    const card = e.target.parentElement.parentElement.parentElement;
-    card.classList.replace('card', 'hide');
-    const form = card.previousElementSibling;
-    form.classList.replace('hide', 'show');
-    console.log(form);
+  showBtns.forEach(showBtn => {
+    showBtn.addEventListener('click', e => {
+      const card = e.target.parentElement.parentElement.parentElement;
+      card.classList.replace('card', 'hide');
+      const form = card.previousElementSibling;
+      form.classList.replace('hide', 'show');
+    });
   });
-});
 
-hideBtns.forEach(hideBtn => {
-  hideBtn.addEventListener('click', e => {
-    const card = e.target.parentElement.parentElement.parentElement;
-    card.classList.replace('hide', 'card');
-    const form = card.previousElementSibling;
-    form.classList.replace('show', 'hide');
-    console.log(form);
+  hideBtns.forEach(hideBtn => {
+    hideBtn.addEventListener('click', e => {
+      const card = e.target.parentElement.parentElement.parentElement;
+      card.classList.replace('hide', 'card');
+      const form = card.previousElementSibling;
+      form.classList.replace('show', 'hide');
+    });
   });
-});
 
-const addBtn = document.querySelector('.add-new-btn');
-const newForm = document.querySelector('.add-new');
+  const addBtn = document.querySelector('.add-new-btn');
+  const newForm = document.querySelector('.add-new');
 
-const arrowLeft = document.querySelector('.arrow-left');
-const arrowRight = document.querySelector('.arrow-right');
-const carousel = document.querySelector('.carousel');
+  const arrowLeft = document.querySelector('.arrow-left');
+  const arrowRight = document.querySelector('.arrow-right');
+  const carousel = document.querySelector('.carousel');
 
-if (arrowLeft && arrowRight && carousel) {
   arrowLeft.onclick = () => {
     if (carousel.scrollLeft === 0) {
       carousel.scrollLeft += 3000;
@@ -45,10 +42,12 @@ if (arrowLeft && arrowRight && carousel) {
     }
     carousel.scrollLeft += carousel.offsetWidth;
   };
+
+  addBtn.addEventListener('click', () => {
+    newForm.classList.toggle('hide');
+    newForm.classList.toggle('show');
+    addBtn.classList.toggle('rotate');
+  });
 }
 
-addBtn.addEventListener('click', () => {
-  newForm.classList.toggle('hide');
-  newForm.classList.toggle('show');
-  addBtn.classList.toggle('rotate');
-});
+window.addEventListener('load', main);
