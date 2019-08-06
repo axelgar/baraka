3fa134c64336e8409f23b99f140fa44b35a415ff
axelgar <axel.garcia94@gmail.com>
2019-08-06 23:21:56 +0200

    add dsisabled buttons

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -8,31 +8,31 @@ function main () {
   arrowLeft.forEach((arrow, index) => {
     arrow.addEventListener('click', () => {
       var numberOfPictures = carousel[index].children[0].children.length - 1;
+      arrow.setAttribute('disabled', true);
 
       if (carousel[index].scrollLeft === 0) {
         carousel[index].scrollLeft += carousel[index].offsetWidth * numberOfPictures;
       }
       carousel[index].scrollLeft -= carousel[index].offsetWidth;
-      arrow.classList.add('freeze');
     });
   });
 
   arrowRight.forEach((arrow, index) => {
     arrow.addEventListener('click', () => {
+      arrow.setAttribute('disabled', true);
       var numberOfPictures = carousel[index].children[0].children.length - 1;
       if (carousel[index].scrollLeft >= (carousel[index].offsetWidth * numberOfPictures)) {
         carousel[index].scrollLeft = 0;
       } else {
         carousel[index].scrollLeft += carousel[index].offsetWidth;
       }
-      arrow.classList.add('freeze');
     });
   });
 
   carousel.forEach((container) => {
     container.addEventListener('scroll', (event) => {
       setTimeout(() => {
-        [...event.target.parentElement.children[0].children].forEach(arrow => arrow.classList.remove('freeze'));
+        [...event.target.parentElement.children[0].children].forEach(arrow => arrow.removeAttribute('disabled'));
       }, 800);
     });
   });
